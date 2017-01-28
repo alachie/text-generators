6e0d7535b5ec1eb59f3fb75fa8ba8c7a1083b1ab
alachie <_@lachie.co>
2017-01-28 17:29:46 +1100

    added github link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => (
 	<footer>
 		<span>written by <a href="http://lachie.co/">lachie.co</a></span>
-		<span><a href="#">github</a></span>
+		<span><a href="https://github.com/alachie/text-generators">github</a></span>
 		<span>2017</span>
 	</footer>
 )
