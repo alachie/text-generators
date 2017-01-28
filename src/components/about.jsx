c55d0eee9426f4258d06293dc79ae12fdfdf3663
alachie <_@lachie.co>
2017-01-28 18:12:37 +1100

    worked on about page

---
+++ src/components/about.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+const About = () => (
+	<div className="about">
+		<h2>About</h2>
+		<p>This app is a collection of text processors that I have written in JavaScript. I originally wrote them as seperate services and websites, but for simplicity and for future expansion, I've decided to combine them into one app.</p>
+		<h2>Glossary</h2>
+		<div class="row">
+			<div class="col">
+				<span className="large">👏</span>
+			</div>
+			<div class="col">
+				<p>Clap, is a processor that replaces spaces in scetences with a clap emoji (<span className="inline-emoji">👏</span>) based on <a href="http://knowyourmeme.com/memes/clap-emoji">this meme</a>.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<span className="large">🎺</span>
+			</div>
+			<div class="col">
+				<p>Toot, is a processor that replaces all regular width characters with <a href="https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms">full width characters</a>.</p>
+			</div>
+		</div>
+	</div>
+)
+export default About
